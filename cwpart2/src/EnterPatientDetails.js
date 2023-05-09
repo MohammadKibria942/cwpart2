fa9660ab78f77beaea9470bcb9d36ebbae48b5ad
BogdanTothazan <102042446+BogdanTothazan@users.noreply.github.com>
2023-05-09 06:07:51 +0100

    W1827155 Umar Hussain - part

---
+++ cwpart2/src/EnterPatientDetails.js
@@ -0,0 +1,29 @@
+import './PatientRecords.css';
+import React from 'react';
+import MyHeader from './Components/MyHeader.js';//Import Header component
+import MyInput from './Components/MyInput.js';//Import Input component
+import MyFooter from './Components/MyFooter.js';//import Footer component
+import BackLink from '@govuk-react/back-link';//import backlink to take back to previous page
+
+
+import { createContext } from 'react';
+export const CurrentContext = createContext(null);
+
+function EnterPatientDetails() {
+
+    return (
+        <>
+            <div class="div2">{/create a div to show all the components/}
+                <MyHeader />
+                <BackLink href="/">Back</BackLink>{/Takes you back to main menu change this/}
+
+                <MyInput />
+                <br />
+                <br />
+
+                <MyFooter />
+            </div>
+        </>
+    )
+}
+export default EnterPatientDetails;
